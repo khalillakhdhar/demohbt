725ef606418f3f6b3c51656323505328f76aa407
khalil <khalillakhdharatc@gmail.com>
2023-04-05 13:26:58 +0100

    jpa

---
+++ src/main/java/com/jpa/model/Employee.java
@@ -0,0 +1,16 @@
+package com.jpa.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee extends User {
+	private String profession;
+	private double salaire;
+
+}
--- src/main/java/com/jpa/model/User.java
+++ src/main/java/com/jpa/model/User.java
@@ -1,8 +1,11 @@
 package com.jpa.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -13,6 +16,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "grade")
 public class User {
 
 	@Id
